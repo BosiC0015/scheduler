a215dfc7e685daea785eb402754bc67dab486ddc
Bosi Chen <chenbosi.bosi@gmail.com>
2021-12-09 17:04:22 -0500

    changing couple of prop names

--- src/components/Application.js
+++ src/components/Application.js
@@ -36,8 +36,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
         <DayList
           days={days}
-          day={selectedDay}
-          setDay={setSelectedDay}
+          value={selectedDay}
+          onChange={setSelectedDay}
         />
         </nav>
         <img
