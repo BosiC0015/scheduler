bf364e1bf83ad1546534dd5d8e18a097b50dcb0f
Bosi Chen <chenbosi.bosi@gmail.com>
2021-12-12 14:15:06 -0500

    get days data from /api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
@@ -44,26 +45,16 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [selectedDay, setSelectedDay] = useState('Monday');
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-
+  useEffect(() => {
+    const daysURL = `http://localhost:8001/api/days`;
+    axios.get(daysURL).then(response => {
+      // console.log(response.data);
+      setDays([...response.data]);
+    })
+  }, [selectedDay])
 
   const appointmentList = appointments.map(appointment => 
       <Appointment 
